83bd812db5f1f2fa57db8ee03436387124452555
Arthur Lee <rley.dev@gmail.com>
2023-03-09 19:33:54 +0600

    feat: implement explore section

--- src/components/explore/CryptoPlanet/Planet/planet.tsx
+++ src/components/explore/CryptoPlanet/Planet/planet.tsx
@@ -4,22 +4,37 @@ import './planet.scss'
 const Planet = () => {
     return (
         <div className="planet">
-            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <circle className="circle" cx="50" cy="50" r="40" fill="transparent"
-                stroke="#E75626"
-                strokeWidth="0.5"
-                strokeDasharray="251.2"
-                strokeDashoffset="251.2">
-                    <animate attributeName="stroke-dashoffset"
-                        dur="2s"
-                        repeatCount="indefinite"
-                        from="251.2"
-                        to="0"
-                        fill="freeze"
-                    />
-                </circle>
-            </svg>
+            <div className="planet__l1">
+                <div className="planet-anim">
+                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                        <circle className="circle" cx="50" cy="50" r="40" fill="transparent"
+                        stroke="#E75626"
+                        strokeWidth="0.5"
+                        strokeDasharray="251.2"
+                        strokeDashoffset="251.2">
+                        
+                            <animate attributeName="stroke-dashoffset"
+                                dur="2s"
+                                repeatCount="indefinite"
+                                from="251.2"
+                                to="0"
+                                fill="freeze"
+                            />
+                        </circle>
+                    </svg>
+
+                </div>
+                <div className="planet__l2">
+                    <div className="planet__l2__stroke1">
+                        <div className="planet__l2__stroke2">
+                            <div className="planet__l2__lay">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
--- src/components/explore/CryptoPlanet/crypto-planet.tsx
+++ src/components/explore/CryptoPlanet/crypto-planet.tsx
@@ -4,10 +4,12 @@ import Planet from './Planet/planet'
 
 const CryptoPlanet = () => {
     return (
-        <div>
-            <div className="headline">
+        <div className="first-headline">
+            <div className="first-headline__headline">
                 <h1>Explore Your own planet</h1>
                 <h1>In <span>our New</span> metaverse</h1>
+                <div className="nplanet">N PLANET</div>
+                <div className="verse">VERSE</div>
             </div>
             <Planet />
             
